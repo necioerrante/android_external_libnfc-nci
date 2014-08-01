30519e80b4131af6f26a19783e3c37f1472b5331
Jizhou Liao <jizhou.liao@nxp.com>
2014-07-31 17:01:23 -0700

    Rename NXP HAL
    Renamed to nfc_nci.nxp54x.default

--- halimpl/pn54x/Android.mk
+++ halimpl/pn54x/Android.mk
@@ -31,7 +31,7 @@ LOCAL_PRELINK_MODULE := false
 LOCAL_ARM_MODE := arm
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
-LOCAL_MODULE := nfc_nci_pn547.$(HAL_SUFFIX)
+LOCAL_MODULE := nfc_nci.pn54x.default
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_SRC_FILES := $(call all-c-files-under, .)  $(call all-cpp-files-under, .)
 LOCAL_SHARED_LIBRARIES := liblog libcutils libhardware_legacy libdl libstlport
